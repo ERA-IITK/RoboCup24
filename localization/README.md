635f23f2a1a78109a7a2e8a46f2dc06810d4c453
Suryansh Goel <sgoel1811@gmail.com>
2023-09-26 08:41:59 +0530

    linedetection_tested

---
+++ localization/README.md
@@ -0,0 +1,10 @@
+# Localisation
+## Getting started 
+In order to run the code, type the following commands in the terminal:
+```
+make
+```
+Then, to run the code, type:
+```
+./localization
+```
--- localization/linedetection.cpp
+++ localization/linedetection.cpp
@@ -1,39 +1,43 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
-using namespace cv;
-using namespace std;
-void whitelinedetection(Mat dewarpedImage){
-  Mat grayImage;
-  cvtColor(dewarpedImage, grayImage, COLOR_BGR2GRAY);
-
-  Mat binaryImage;
-  threshold(grayImage, binaryImage, 200, 255, THRESH_BINARY);
-
-  vector<vector<Point>> contours;
-  findContours(binaryImage, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-
-  vector<vector<Point>> whiteLinesCoordinates;
-  for (const auto& contour : contours) {
-      whiteLinesCoordinates.push_back(contour);
-  }
-
-  for (size_t i = 0; i < whiteLinesCoordinates.size(); i++) {
-      cout << "White Line " << i + 1 << " Coordinates:" << endl;
-      for (const auto& point : whiteLinesCoordinates[i]) {
-          cout << "(" << point.x << ", " << point.y << ")" << endl;
-      }
-  }
-
-}
 int main() {
-    Mat dewarpedImage = imread("dewarped_image.jpg");
+    // Load the image
+    cv::Mat image = cv::imread("fisheye_field.jpg");
 
-    if (dewarpedImage.empty()) {
-        cerr << "Error: Unable to load image." << endl;
+    if (image.empty()) {
+        std::cerr << "Error: Unable to load image." << std::endl;
         return -1;
     }
-    whitelinedetection(dewarpedImage);
-    
+
+    // Convert the image to grayscale
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+
+    // Threshold the image to create a binary mask of white regions
+    cv::Mat binary;
+    cv::threshold(gray, binary, 200, 255, cv::THRESH_BINARY);
+
+    // Find contours in the binary image
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(binary, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+    // Filter and draw the detected white lines (be lenient)
+    cv::Mat resultImage = image.clone();
+    for (const auto& contour : contours) {
+        // Filter white contours (lines) based on area (adjust the area threshold as needed)
+        if (cv::contourArea(contour) > 25) { // Adjust the area threshold as needed for line detection
+            cv::drawContours(resultImage, std::vector<std::vector<cv::Point>>{contour}, -1, cv::Scalar(0, 255, 0), 2); // Draw white contours (lines) in green
+        }
+    }
+
+    // Display the result
+    cv::imshow("White Lines Detection", resultImage);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+
+    // Print the number of white lines detected
+    std::cout << "Number of white lines detected: " << contours.size() << std::endl;
+
     return 0;
 }
