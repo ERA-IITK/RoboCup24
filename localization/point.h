8958c08a10a813df842b406752a1e28e7a0398d6
Suryansh Goel <sgoel1811@gmail.com>
2023-10-14 03:42:37 +0530

    point.h added

--- localization/point.h
+++ localization/point.h
@@ -1,9 +1,15 @@
 // point.h
 
 #pragma once  // This ensures that the header is included only once
+struct WPoint {
+    double x;
+    double y;
+};
 
 struct Point {
     double x;
     double y;
     double theta;
+    vector<WPoint> wlp;
+    vector<WPoint> nlp;
 };
