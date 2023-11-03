65faf2919e786bfd48c5950555dba3d19e37a60d
LegendaryGene <rishi21@iitk.ac.in>
2023-11-04 05:18:10 +0530

    Modified to enable simulation testing

--- localization/linedetection.cpp
+++ localization/linedetection.cpp
@@ -1,16 +1,8 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-int main() {
-    // Load the image
-    cv::Mat image = cv::imread("./images/fisheye_field2.jpg");
+std::vector<std::vector<cv::Point>> linedetection(cv::Mat image) {
 
-    if (image.empty()) {
-        std::cerr << "Error: Unable to load image." << std::endl;
-        return -1;
-    }
-    //print cv mat image
-    
     // Convert the image to HSV color space
     cv::Mat hsv;
     cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
@@ -29,20 +21,19 @@ int main() {
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(greenMask, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
-    // Create an output image to draw the contours
-    cv::Mat resultImage = image.clone();
+    // // Create an output image to draw the contours
+    // cv::Mat resultImage = image.clone();
 
-    // Draw all detected contours in blue
-    cv::drawContours(resultImage, contours, -1, cv::Scalar(255, 0, 0), 1); // Blue color (BGR)
+    // // Draw all detected contours in blue
+    // cv::drawContours(resultImage, contours, -1, cv::Scalar(255, 0, 0), 1); // Blue color (BGR)
 
-    // Display the result image
-    cv::imshow("All Contours Detection", resultImage);
-    cv::waitKey(0);
-    cv::imwrite("./images/fisheye_field2Contour.jpg", resultImage);
-    cv::destroyAllWindows();
+    // // Display the result image
+    // cv::imshow("All Contours Detection", resultImage);
+    // cv::waitKey(0);
+    // cv::imwrite("./images/fisheye_field2Contour.jpg", resultImage);
+    // cv::destroyAllWindows();
 
-    // Print the number of contours detected
-    std::cout << "Number of contours detected: " << contours.size() << std::endl;
-
-    return 0;
+    // // Print the number of contours detected
+    // std::cout << "Number of contours detected: " << contours.size() << std::endl;
+    return contours;
 }
