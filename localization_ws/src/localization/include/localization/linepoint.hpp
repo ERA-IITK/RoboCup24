feeb19428e8c7971d738f11f378f4d48f0ef1622
surya2003-real <sgoel1811@gmail.com>
2024-01-11 01:51:38 +0530

    includes new imports

---
+++ localization_ws/src/localization/include/localization/linepoint.hpp
@@ -0,0 +1,7 @@
+// point.hpp
+#pragma once
+
+struct linepoint{                                    
+    float x,y,d;
+    // point at which vectors sent from the point in consideraton intersect the line
+};
---
+++ localization_ws/src/localization/include/localization/raw_points.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+struct raw_pt{
+    float x_angle, y_angle, depth;
+};
